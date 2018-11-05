bad1af3ca9d28aad90d3f2a79d97e8e1cc10965d
AlgorithChang <1361372700@qq.com>
2018-11-05 22:13:34 +0800

    create login page

--- final_exam/final_exam/settings.py
+++ final_exam/final_exam/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+from django.conf.global_settings import STATICFILES_DIRS, AUTH_USER_MODEL
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -37,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'wechat',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +57,7 @@ ROOT_URLCONF = 'final_exam.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "wechat", "static"),
+)
+
+
+AUTH_USER_MODEL = 'wechat.MyUser'
--- final_exam/wechat/views.py
+++ final_exam/wechat/views.py
@@ -1,3 +1,28 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.contrib import auth
 
 # Create your views here.
+from .forms import LoginForm
+
+
+def index_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            user = auth.authenticate(request, username=username, password=password)
+            if user is not None and user.is_active:
+                # Correct password, and the user is marked "active"
+                auth.login(request, user)
+                # Redirect to a success page.
+                return HttpResponseRedirect("me?user=" + username)
+            else:
+                error_message = "Sorry, that's not a valid username or password"
+                return render(request, 'wechat/login.html',
+                              {'form': form, 'input_error': error_message, 'block_title': 'Login'})
+    else:
+        form = LoginForm()
+    return render(request, 'wechat/login.html',
+                  {'form': form, 'block_title': 'Login'})
