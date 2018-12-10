1f210edbeecdd7ad2f87bbb0c07d0accc095bbed
cw <296083364@qq.com>
2018-12-10 17:21:35 +0800

    version 1.5 requirements.txt

--- final_exam/wechat/models.py
+++ final_exam/wechat/models.py
@@ -49,6 +49,12 @@ class MyUser(AbstractBaseUser):
         max_length=255,
         unique=True,
     )
+    sex = models.CharField(
+        verbose_name='sex',
+        max_length=10,
+        null=True
+    )
+
     created_at = models.DateField()
     is_active = models.BooleanField(default=True)
     is_admin = models.BooleanField(default=False)
@@ -82,3 +88,58 @@ class MyUser(AbstractBaseUser):
     def set_created_date(self):
         self.created_at = datetime.date.today()
 
+
+class User_realation(models.Model):
+    uid = models.ForeignKey(MyUser, related_name='uid_UR', on_delete=models.CASCADE)
+    friend_id = models.ForeignKey(MyUser, related_name='friend_id_UR', on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = (('uid', 'friend_id'),)
+
+    primary = ('uid', 'friend_id')
+
+    def __str__(self):
+        return "User_realation"
+
+
+class One_to_one_msg_record(models.Model):
+    mid = models.IntegerField(primary_key=True)
+    form_id = models.ForeignKey(MyUser, related_name='from_id_O2O', on_delete=models.CASCADE)
+    to_id = models.ForeignKey(MyUser, related_name='to_id_O2O', on_delete=models.CASCADE)
+    content = models.CharField(max_length=500)
+
+    def __str__(self):
+        return "One_to_one_msg_record"
+
+
+class Group(models.Model):
+    gid = models.IntegerField(primary_key=True)
+    gname = models.CharField(max_length=20)
+    create_time = models.DateField(auto_now_add=True)
+    num_of_group = models.IntegerField()
+    master_id = models.ForeignKey(MyUser, on_delete=models.CASCADE)
+
+
+class Group_msg(models.Model):
+    mid = models.IntegerField()
+    form_id = models.ForeignKey(MyUser, related_name='from_id_GM', on_delete=models.CASCADE)
+    gid = models.ForeignKey(Group, related_name='gid_GM', on_delete=models.CASCADE)
+    content = models.CharField(max_length=500)
+    create_time = models.DateField(auto_now_add=True)
+
+    class Meta:
+        unique_together = (('mid', 'gid'),)
+
+    primary = ('mid', 'gid')
+
+
+class G_msg_config(models.Model):
+    uid = models.ForeignKey(MyUser, related_name='uid_GMC', on_delete=models.CASCADE)
+    gid = models.ForeignKey(Group, related_name='gid_GMC', on_delete=models.CASCADE)
+    last_read_msg_id = models.ForeignKey(Group_msg, related_name='last_read_msg_id', on_delete=models.CASCADE,
+                                         null=False)
+
+    class Meta:
+        unique_together = (('uid', 'gid'),)
+
+    primary = ('uid', 'gid')
