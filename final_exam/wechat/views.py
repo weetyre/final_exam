7494070f98e98cd84b660eecfb41c3c5f84d27d1
cw <296083364@qq.com>
2018-12-14 02:27:49 +0800

    version 2.1 bug fixes

--- final_exam/wechat/views.py
+++ final_exam/wechat/views.py
@@ -1,5 +1,6 @@
 from django.contrib import auth
 from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.backends import ModelBackend
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.hashers import make_password
 from django.http import HttpResponse, Http404, HttpResponseRedirect
@@ -8,34 +9,48 @@ from django.shortcuts import render, render_to_response
 from .admin import UserCreationForm
 from .forms import LoginForm, ChangeEmailForm, MyPasswordChangeForm
 from .models import MyUser
+from django.db.models import Q
+from django.contrib.auth import authenticate
 
 
 def index_login(request):
     if request.method == 'POST':
         form = LoginForm(request.POST)
         if form.is_valid():
-            email = form.cleaned_data['email']
+            text = form.cleaned_data['text']
             password = form.cleaned_data['password']
 
-            # if email is not found
-            flag = 1
-            user_model = MyUser.objects.all()
-            for i in user_model:
-                if i.email == email:
-                    flag = 0
+            # # if email is not found
+            user_model = None
+            try:
+                user_model = MyUser.objects.get(email=text)
+            except Exception as e:
+                pass
 
-            if flag:
-                error_message = 'email not found.'
-                return render(request, 'login.html',
-                              {'form': form, 'input_error': error_message, 'block_title': 'Login'})
+            if user_model:
+                user = authenticate(request=request, username=text, password=password)
+                # incorrect password
+                if user is None:
+                    error_message = 'password is invalid.'
+                    return render(request, 'login.html',
+                                  {'form': form, 'input_error': error_message, 'block_title': 'Login'})
+            else:
+                try:
+                    user_model = MyUser.objects.get(username=text)
+                except Exception as e:
+                    pass
 
-            # user = MyUser.objects.get(email=email)
-            user = auth.authenticate(email=email, password=password)
-            # incorrect password
-            if user is None:
-                error_message = 'password is invalid.'
-                return render(request, 'login.html',
-                              {'form': form, 'input_error': error_message, 'block_title': 'Login'})
+                if user_model:
+                    user = authenticate(request=request, username=text, password=password)
+                    # incorrect password
+                    if user is None:
+                        error_message = 'password is invalid.'
+                        return render(request, 'login.html',
+                                      {'form': form, 'input_error': error_message, 'block_title': 'Login'})
+                else:
+                    error_message = 'email or username not found.'
+                    return render(request, 'login.html',
+                                  {'form': form, 'input_error': error_message, 'block_title': 'Login'})
 
             if user is not None and user.is_active:
                 # Correct password, and the user is marked "active"
@@ -64,17 +79,18 @@ def index_register(request):
             email = request.POST['email']
             sex = request.POST['sex']
             # password = make_password(form.cleaned_data['password'])
-            user = MyUser.objects.create_user(username, email, sex, request.POST['password'])
+            user = MyUser.objects.create_user(username=username, email=email, sex=sex,
+                                              password=request.POST['password'])
 
-            user = auth.authenticate(email=email, password=request.POST['password'])
+            user = authenticate(request=request, username=email, password=request.POST['password'])
             auth.login(request, user)
 
-            return HttpResponseRedirect("/profile?email=" + user.email)
+            return HttpResponseRedirect("/home")
         else:
             if len(email_filter) > 0:
                 error_msg1 = 'email already taken.'
                 return render(request, 'register.html',
-                              {'form': form, 'input_error': error_msg1, 'block_title': 'Register'})
+                              {'form': form, 'input_error2': error_msg1, 'block_title': 'Register'})
 
             if len(username_filter) > 0:
                 error_msg2 = 'username already taken.'
@@ -150,7 +166,20 @@ def index_logout(request):
     auth.logout(request)
     return HttpResponseRedirect("/login")
 
+
 @login_required
 def myhome(request):
     if request.method == 'GET':
         return render(request, 'home_base.html', {'user': request.user})
+
+
+class CustomBackend(ModelBackend):
+    """邮箱也能登录"""
+
+    def authenticate(self, request, username=None, password=None, **kwargs):
+        try:
+            user = MyUser.objects.get(Q(username=username) | Q(email=username))
+            if user.check_password(password):
+                return user
+        except Exception as e:
+            return None
