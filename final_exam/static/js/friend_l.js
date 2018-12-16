b8b69a5afb71e170a0cfb98690a0402df39e52a6
cw <296083364@qq.com>
2018-12-16 18:59:38 +0800

    version 2.14

---
+++ final_exam/static/js/friend_l.js
@@ -0,0 +1,12 @@
+$('#list-tab').click(function () {
+    massage_box = document.getElementById('message')
+})
+
+$('#groups-tab').click(function () {
+    massage_box = document.getElementById('message')
+    massage_box.innerHTML=""
+})
+
+$('#friends-tab').click(function () {
+    massage_box = document.getElementById('message')
+})
