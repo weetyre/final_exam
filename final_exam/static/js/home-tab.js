d10e0814e1f8f2bf046efa2f95bbecb4638a5586
AlgorithChang <1361372700@qq.com>
2018-12-19 22:34:34 +0800

    append msg

--- final_exam/static/js/home-tab.js
+++ final_exam/static/js/home-tab.js
@@ -17,7 +17,14 @@ function listItemClick(e) {
 let uid = '1';
 
 function append_msg(msg) {
-    if (msg['from'] == uid) {
+    let from = msg["from"];
+    //let msg_show = document.getElementById("msg-show");
+    var html_div_content;
 
+    if (msg['from'] == uid) {
+        html_div_content = '<div class="media text-muted py-2 msg-item msg-mine" data-userid="' + from + '"><div class="d-flex msg-container"><img data-src="" alt="32x32" class="rounded img-face-msg" src="" data-holder-rendered="true"><p class="media-body mb-0 small lh-125 p-2 msg-content">' + msg["content"] + '</p></div></div>'
+    }else{
+        html_div_content = '<div class="media text-muted py-2 msg-item msg-other" data-userid="' + from + '"><div class="d-flex msg-container"><img data-src="" alt="32x32" class="rounded img-face-msg" src="" data-holder-rendered="true"><p class="media-body mb-0 small lh-125 p-2 msg-content">' + msg["content"] + '</p></div></div>'
     }
+    $('#msg-show').append(html_div_content)
 }
